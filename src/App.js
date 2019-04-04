ba6610fbdd42db5c489613dffae0fb3caa0cfbbc
Thomas Stang <tstang@mbpr-tstang-sf.sugarinc.com>
2019-04-03 21:27:25 -0700

    Add initial app data fetching set up

--- src/App.js
+++ src/App.js
@@ -1,31 +1,47 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.scss';
-import page1 from './data/test-data-page-1.json';
+// import page1 from './data/test-data-page-1.json';
 
 
-class App extends Component {
-  render() {
-    console.log(page1);
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const App = () => {
+  const [results, setResults] = useState([]);
+  const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [totalResults, setTotalResults] = useState(0);
+  const [search, setSearch] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    // Setting this up to get data on mount, this will be moved to happen on search keydown
+    axios.get('data/test-data-page-1.json')
+      .then(res => {
+        const data = res.data;
+        setResults([results, ...data.results]);
+        setLoading(false);
+      })
+  }, [search])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
