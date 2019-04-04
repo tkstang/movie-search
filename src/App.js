13f51da02bbd345cc4b5fb549849f6f0dc3f71dc
Thomas Stang <tstang@mbpr-tstang-sf.sugarinc.com>
2019-04-03 20:52:45 -0700

    Add test data for development without internet connection

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.scss';
+import page1 from './data/test-data-page-1.json';
+
 
 class App extends Component {
   render() {
+    console.log(page1);
     return (
       <div className="App">
         <header className="App-header">
